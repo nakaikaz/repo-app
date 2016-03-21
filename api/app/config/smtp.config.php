e8c93dc23e5b8330f74d7023b0bd161f9064ad63
nakaikaz <nakaikaz@gmail.com>
2016-03-21 21:10:49 +0900

    added smtp.config.php

---
+++ api/app/config/smtp.config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'host' => 'smtp.lolipop.jp',
+	'user' => 'noreply@nakaikaz.boy.jp',
+	'pass' => 'ldeuofrt12631ber',
+	'port' => 465,
+	'sender' => array(
+		'email' => 'noreply@nakaikaz.boy.jp',
+		'name' => 'repo-app'
+	),
+);
